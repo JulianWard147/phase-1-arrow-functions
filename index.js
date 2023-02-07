d70914f5c4ff77abbfd95bbf98a0c59e913818ad
Julian Ward <wardjulianm@gmail.com>
2023-02-07 11:23:41 -0700

    Okay, I think I get the syntax. Still not sure why to use one and not other

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide = function(a,b){
+    return (a/b)
+}
+
+console.log(divide(30,5)) // 6
+
+const square = a => a*a
+
+console.log(square(5)) // 25
+
+const add = (a, b) => a+b
+
+console.log(add(30,5)) //35
